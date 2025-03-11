8da204ac7276a1b13cd428ae5818c6d7b317cdc4
Priyanshu7129 <priyanshu7129@gmail.com>
2025-03-11 22:56:44 +0530

    Implemented UC6: Used lombok to auto-generate getters, setters and constructors

--- src/main/java/com/example/addressbook/dto/AddressBookDTO.java
+++ src/main/java/com/example/addressbook/dto/AddressBookDTO.java
@@ -1,49 +1,15 @@
 package com.example.addressbook.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data  // Generates Getters, Setters, toString, equals, and hashCode
+@NoArgsConstructor  // Generates a No-Args Constructor
+@AllArgsConstructor  // Generates an All-Args Constructor
 public class AddressBookDTO {
-    private Long id;  // Add this field
+    private Long id;
     private String name;
     private String phoneNumber;
     private String email;
-
-    public AddressBookDTO() {}
-
-    public AddressBookDTO(Long id, String name, String phoneNumber, String email) {
-        this.id = id;
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
--- src/main/java/com/example/addressbook/model/AddressBook.java
+++ src/main/java/com/example/addressbook/model/AddressBook.java
@@ -1,50 +1,15 @@
 package com.example.addressbook.model;
 
-public class AddressBook {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data  // Generates Getters, Setters, toString, equals, and hashCode
+@NoArgsConstructor  // Generates a No-Args Constructor
+@AllArgsConstructor  // Generates an All-Args Constructor
+public class AddressBook {
     private Long id;
     private String name;
     private String phoneNumber;
     private String email;
-
-    public AddressBook() {}
-
-    public AddressBook(Long id, String name, String phoneNumber, String email) {
-        this.id = id;
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
